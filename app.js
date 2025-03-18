020bef3def30ce3ff0ae2351b6b787f404a780a1
Retromantis <victorzegarramendoza@gmail.com>
2025-03-18 00:20:24 -0500

    creación de pagina web del juego amigo secreto

---
+++ app.js
@@ -0,0 +1,44 @@
+
+let nombresIngresados = [];
+let mensajeDeAlerta = "Por favor, ingrese mas nombres";
+
+function agregarAmigo() {
+    let entrada = document.getElementById("amigo").value;
+    let valorEntrada = entrada.trim();
+    if (valorEntrada === "") {
+        alert(mensajeDeAlerta);
+    } else {
+        nombresIngresados.push(valorEntrada);
+        document.getElementById("amigo").value = "";
+        actualizarListaAmigos();
+    }
+}
+
+function actualizarListaAmigos() {
+    document.getElementById("resultado").innerHTML = "";
+    let lista = document.getElementById("listaAmigos")
+    lista.innerHTML = "";
+    for (let i = 0; i < nombresIngresados.length; i++) {
+        let elementoLista = document.createElement("li");
+        elementoLista.textContent = nombresIngresados[i];
+        lista.appendChild(elementoLista);
+    }
+}
+
+function sortearAmigo() {
+    if (nombresIngresados.length === 0) {
+        alert(mensajeDeAlerta);
+    } else {
+        let indiceAleatorio = Math.floor(Math.random() * nombresIngresados.length);
+        let nombreSorteado = nombresIngresados[indiceAleatorio];
+        document.getElementById("resultado").innerHTML = `${nombreSorteado} es tu amigo secreto `;
+        borrarLista();
+    }
+
+}
+
+function borrarLista() {
+    nombresIngresados = [];
+    document.getElementById("listaAmigos").innerHTML = "";
+    document.getElementById("amigo").value = "";
+}
